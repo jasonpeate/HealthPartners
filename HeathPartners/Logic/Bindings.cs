7ea083ef035e3ba0795c538ee5aa8c9d2db1c227
jason.peate <jason.peate@ematics.co.uk>
2023-07-12 21:39:43 +0100

    adding in console layer

---
+++ HeathPartners/Logic/Bindings.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Logic
+{
+    public static class Bindings
+    {
+        public static void SetupBindings(ServiceCollection ServiceCollection)
+        {
+            ServiceCollection.AddScoped<IFizzBuzzLogic, FizzBuzzLogic>();
+        }
+    }
+}
--- HeathPartners/Presentation/Program.cs
+++ HeathPartners/Presentation/Program.cs
@@ -1,2 +1,62 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using Logic;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+class Program
+{
+    private static ServiceProvider? serviceProvider;
+    private static ILogger? logger;
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Fizz Buzz, Please press any key to process");
+        Setup();
+        logger.LogDebug("Setup has complete");
+
+        Console.ReadLine();
+
+        logger.LogInformation("Executing FizzBuzz between 1 and 100");
+
+        List<object> input = Enumerable.Range(1, 100).Select(a => (object)a).ToList();
+
+        IFizzBuzzLogic instance = serviceProvider.GetService<IFizzBuzzLogic>();
+
+        logger.LogDebug("Completed Processing");
+
+        List<string> result = instance.Solve(input);
+
+        result.ForEach(a =>
+        {
+            Console.WriteLine(a);
+        });
+
+        Console.WriteLine("Processing Complete, Press any key to exit");
+
+        Console.WriteLine("Thank you for your time!");
+
+        Console.ReadLine();
+
+    }
+
+    static void Setup()
+    {
+        ServiceCollection sc = new ServiceCollection();
+        Bindings.SetupBindings(sc);
+
+        sc.AddLogging((loggingBuilder) => loggingBuilder
+                                    .SetMinimumLevel(LogLevel.Trace)
+                                    .AddConsole()
+                        );
+
+        serviceProvider = sc.BuildServiceProvider();
+
+
+        logger = serviceProvider.GetService<ILoggerFactory>()
+            .CreateLogger<Program>();
+
+
+    }
+}
